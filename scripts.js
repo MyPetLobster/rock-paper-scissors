0bb0922b376f7881902a76fe45ad0c74380cdfe9
MyPetLobster <6979547+MyPetLobster@users.noreply.github.com>
2023-11-16 18:50:27 -0500

    Clean up code, add comments

--- scripts.js
+++ scripts.js
@@ -1,90 +1,69 @@
-// getComputerChoice() will randomly return 'Rock', 'Paper'
-// or 'Scissors'
-
-// Write function that plays a single round of RCP
-// func should take 2 param -- playerSelection, computerSelection
-// and return a string that declares the winner of a round
-// "You lose! Paper beats Rock"
-// ** case insensitive playerSelection
-
-// Write a new function called game(). Use prev func
-// 5 round game
-
+// Array representing the possible choices in Rock, Paper, Scissors
 const rockPaperScissors = ['rock', 'paper', 'scissors'];
 
+// Function to get a random choice for the computer
 function getComputerChoice() {
     const index = Math.floor(Math.random() * 3);
     return rockPaperScissors[index];
 }
 
+// Function to get and validate the player's choice
 function getPlayerChoice() {
-    const choice = prompt("Pick rock, paper, or scissors.", "").toLowerCase();
-    if (choice !== "rock" && choice !== "paper" && choice != "scissors") {
-        return "Invalid input";
-    }
-    else {
-        return choice;
+    let isValid = false;
+    while (!isValid) {
+        const choice = prompt("Pick rock, paper, or scissors.", "").toLowerCase();
+        if (rockPaperScissors.includes(choice)) {
+            isValid = true;
+            return choice;
+        } else {
+            alert("Invalid input. Please choose rock, paper, or scissors.");
+        }
     }
 }
 
+// Function to play a single round of Rock, Paper, Scissors
 function round() {
     const playerSelection = getPlayerChoice();
     const computerSelection = getComputerChoice();
-    if (playerSelection === "rock") {
-        if (computerSelection === "rock") {
-            return "Draw -- You both threw Rock";
-        } else if (computerSelection === "paper") {
-            return "You lose! Paper beats Rock."; 
-        } else {
-            return "You win! Rock beats Scissors.";
-        }
-    } else if (playerSelection === "paper") {
-        if (computerSelection === "rock") {
-            return "You win! Paper beats Rock.";
-        } else if (computerSelection === "paper") {
-            return "Draw -- You both threw paper"; 
-        } else {
-            return "You lose! Scissors beats Paper.";
-        }
-    } else if (playerSelection === "scissors") {
-        if (computerSelection === "rock") {
-            return "You lose! Rock beats Scissors.";
-        } else if (computerSelection === "paper") {
-            return "You win! Scissors beats Paper."; 
-        } else {
-            return "Draw -- You both threw scissors";
-        }
+
+    if (playerSelection === computerSelection) {
+        return `Draw -- You both threw ${playerSelection}`;
+    } else if (
+        (playerSelection === "rock" && computerSelection === "scissors") ||
+        (playerSelection === "paper" && computerSelection === "rock") ||
+        (playerSelection === "scissors" && computerSelection === "paper")
+    ) {
+        return `You win! ${playerSelection} beats ${computerSelection}.`;
+    } else {
+        return `You lose! ${computerSelection} beats ${playerSelection}`;
     }
 }
 
-
+// Function to play the game for a specified number of rounds
 function game() {
     let playerWins = 0;
     let computerWins = 0;
     let rndCount = 1;
+
+    // Play until one side wins three rounds
     while (playerWins < 3 && computerWins < 3) {
         let rndResults = round();
         console.log(`Round ${rndCount}: ${rndResults}`);
- 
 
+        // Update scores based on the round results
         if (rndResults.startsWith("You win")) {
             playerWins += 1;
         } else if (rndResults.startsWith("You lose")) {
             computerWins += 1;
         }
 
-        console.log(`You: ${playerWins}\nComputer: ${computerWins}\n`)
+        console.log(`You: ${playerWins}\nComputer: ${computerWins}\n`);
+        rndCount += 1;
     }
-    
-    if (playerWins === 3) {
-        return "You win!!";
-    } else {
-        return "You lose!!";
-    }
+
+    // Determine the overall winner of the game
+    return playerWins === 3 ? "You win!!" : "You lose!!";
 }
 
+// Start the game and display the final result
 console.log(game());
-
-
-
-
