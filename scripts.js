bbc6420569e130ad47acdf2a1395d9eac770c8ec
MyPetLobster <6979547+MyPetLobster@users.noreply.github.com>
2023-11-19 16:54:36 -0500

    Layout html and basic css styling

--- scripts.js
+++ scripts.js
@@ -7,20 +7,6 @@ function getComputerChoice() {
     return rockPaperScissors[index];
 }
 
-// Function to get and validate the player's choice
-function getPlayerChoice() {
-    let isValid = false;
-    while (!isValid) {
-        const choice = prompt("Pick rock, paper, or scissors.", "").toLowerCase();
-        if (rockPaperScissors.includes(choice)) {
-            isValid = true;
-            return choice;
-        } else {
-            alert("Invalid input. Please choose rock, paper, or scissors.");
-        }
-    }
-}
-
 // Function to play a single round of Rock, Paper, Scissors
 function round() {
     const playerSelection = getPlayerChoice();
@@ -39,36 +25,63 @@ function round() {
     }
 }
 
-// Function to play the game for a specified number of rounds
-function game() {
-    let playerWins = 0;
-    let computerWins = 0;
-    let rndCount = 1;
 
-    alert("Best of five!");
 
-    // Play until one side wins three rounds
-    while (playerWins < 3 && computerWins < 3) {
-        let rndResults = round();
 
-        // Update scores based on the round results
-        if (rndResults.startsWith("You win")) {
-            playerWins += 1;
-        } else if (rndResults.startsWith("You lose")) {
-            computerWins += 1;
-        }
-        console.log(`Round ${rndCount}: ${rndResults}`);
-        console.log(`You: ${playerWins}\nComputer: ${computerWins}\n`);
-        alert(`You: ${playerWins}\nComputer: ${computerWins}\n\nRound ${rndCount}: ${rndResults}`)
-        rndCount += 1;
-    }
 
-    // Determine the overall winner of the game
-    return playerWins === 3 ? "You win!!" : "You lose!!";
-}
 
-// Start the game and display the final result
-result = game();
-console.log(result);
-alert(result);
+
+
+
+
+
+// // Function to get and validate the player's choice
+// function getPlayerChoice() {
+//     let isValid = false;
+//     while (!isValid) {
+//         const choice = prompt("Pick rock, paper, or scissors.", "").toLowerCase();
+//         if (rockPaperScissors.includes(choice)) {
+//             isValid = true;
+//             return choice;
+//         } else {
+//             alert("Invalid input. Please choose rock, paper, or scissors.");
+//         }
+//     }
+// }
+
+
+
+
+// // Function to play the game for a specified number of rounds
+// function game() {
+//     let playerWins = 0;
+//     let computerWins = 0;
+//     let rndCount = 1;
+
+//     alert("Best of five!");
+
+//     // Play until one side wins three rounds
+//     while (playerWins < 3 && computerWins < 3) {
+//         let rndResults = round();
+
+//         // Update scores based on the round results
+//         if (rndResults.startsWith("You win")) {
+//             playerWins += 1;
+//         } else if (rndResults.startsWith("You lose")) {
+//             computerWins += 1;
+//         }
+//         console.log(`Round ${rndCount}: ${rndResults}`);
+//         console.log(`You: ${playerWins}\nComputer: ${computerWins}\n`);
+//         alert(`You: ${playerWins}\nComputer: ${computerWins}\n\nRound ${rndCount}: ${rndResults}`)
+//         rndCount += 1;
+//     }
+
+//     // Determine the overall winner of the game
+//     return playerWins === 3 ? "You win!!" : "You lose!!";
+// }
+
+// // Start the game and display the final result
+// result = game();
+// console.log(result);
+// alert(result);
 
